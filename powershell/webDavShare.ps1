c5f6798e6c9b8fe6abd91196ba50ada40d43aa18
Ricardo Juhasz <ricardo.juhasz@maastrichtuniversity.nl>
2023-03-09 14:03:11 +0100

    Delete powershell script [DHDO-731]

--- powershell/webDavShare.ps1
+++
@@ -1,4 +0,0 @@
-[String]$WebDAVShare = '\\download.datahubmaastricht.nl'
-$username = "[System.Environment]::GetEnvironmentVariable('Username')"
-$token = "[System.Environment]::GetEnvironmentVariable('Token')"
-New-PSDrive -Name S -PSProvider FileSystem -Root $WebDAVShare -Credential $username, $token
